364cd96dcf2dc44e834a7f119294d5b7831fe7f4
Jonathan Doucette <jdoucette@physics.ubc.ca>
2023-08-31 16:55:32 -0700

    warn user if multiple `julia` paths found

--- api/jlcall.m
+++ api/jlcall.m
@@ -1,50 +1,50 @@
 function varargout = jlcall(varargin)
 % JLCALL Call Julia from MATLAB using a Julia daemon launched by <a href="matlab: web('https://github.com/dmolina/DaemonMode.jl')">DaemonMode.jl</a>.
 % Find the full documentation at the <a href="matlab: web('https://github.com/jondeuce/MATDaemon.jl')">MATDaemon.jl GitHub repository</a>.
-% 
+%
 % ## Quickstart
-% 
+%
 % Use the MATLAB function JLCALL to call Julia from MATLAB:
-% 
+%
 %   >> JLCALL('sort', {rand(2,5)}, struct('dims', int64(2)))
-%   
+%
 %   ans =
-%   
+%
 %       0.1270    0.2785    0.6324    0.8147    0.9575
 %       0.0975    0.5469    0.9058    0.9134    0.9649
-% 
+%
 % The positional arguments passed to JLCALL are:
 % 1. The Julia function to call, given as a MATLAB char array. This can be any Julia expression which evaluates to a function.
 %    For example, 'a=2; b=3; x -> a*x+b'. For convenience, the empty string '' is interpreted as '(args...; kwargs...) -> nothing', returning nothing for any inputs.
 %    NOTE: expressions are wrapped in a let block and evaluated in the global scope
 % 2. Positional arguments, given as a MATLAB cell array. For example, args = {arg1, arg2, ...}
 % 3. Keyword arguments, given as a MATLAB struct. For example, kwargs = struct('key1', value1, 'key2', value2, ...)
-% 
+%
 % The first time JLCALL is invoked:
 % 1. MATDaemon.jl will be installed into a local Julia project, if one does not already exist. By default, a folder .jlcall is created in the same folder as JLCALL
 % 2. A Julia server will be started in the background using DaemonMode.jl
-% 
+%
 % All subsequent calls to Julia are run on the Julia server.
 % The server will be automatically killed when MATLAB exits.
-% 
+%
 % ### Restarting the Julia server
-% 
+%
 % In the event that the Julia server reaches an undesired state, the server can be restarted by passing the 'restart' flag with value true:
-% 
+%
 %   >> JLCALL('', 'restart', true) % restarts the Julia server and returns nothing
-% 
+%
 % Similarly, one can shutdown the Julia server without restarting it:
-% 
+%
 %   >> JLCALL('', 'shutdown', true) % shuts down the Julia server and returns nothing
-% 
+%
 % ### Setting up the Julia environment
-% 
+%
 % Before calling Julia functions, it may be necessary or convenient to first set up the Julia environment.
 % For example, one may wish to activate a local project environment, run setup scripts, import modules for later use,
 % or set the number of threads for running multithreaded code.
-% 
+%
 % This setup can be conveniently executed at the start of your MATLAB script with a single call to JLCALL as follows:
-% 
+%
 %   >> JLCALL('', ...
 %      'project', '/path/to/MyProject', ... % activate a local Julia Project
 %      'setup', '/path/to/setup.jl', ... % run a setup script to load some custom Julia code
@@ -52,179 +52,179 @@ function varargout = jlcall(varargin)
 %      'threads', 'auto', ... % use the default number of Julia threads
 %      'restart', true ... % start a fresh Julia server environment
 %      )
-% 
+%
 % See the corresponding sections below for more details about these flags.
-% 
+%
 % ### Julia multithreading
-% 
+%
 % The number of threads used by the Julia server can be set using the 'threads' flag:
-% 
+%
 %   >> JLCALL('() -> Threads.nthreads()', 'threads', 8, 'restart', true)
-%   
+%
 %   ans =
-%   
+%
 %     int64
-%   
+%
 %      8
-% 
+%
 % The default value for 'threads' is 'auto', deferring to Julia to choose the number of threads.
-% 
+%
 % NOTE: Julia cannot change the number of threads at runtime.
 % In order for the 'threads' flag to take effect, the server must be restarted.
-% 
+%
 % ### Loading modules
-% 
+%
 % Julia modules can be loaded and used:
-% 
+%
 %   >> JLCALL('LinearAlgebra.norm', {[3.0; 4.0]}, 'modules', {'LinearAlgebra'})
-%   
+%
 %   ans =
-%   
+%
 %        5
-% 
+%
 % NOTE: modules are loaded using import, not using. Module symbols must therefore be fully qualified, e.g. LinearAlgebra.norm in the above example as opposed to norm.
-% 
+%
 % ### Persistent environments
-% 
+%
 % By default, previously loaded Julia code is available on subsequent calls to JLCALL.
 % For example, following the above call to LinearAlgebra.norm, the LinearAlgebra.det function can be called without loading LinearAlgebra again:
-% 
+%
 %   >> JLCALL('LinearAlgebra.det', {[1.0 2.0; 3.0 4.0]})
-%   
+%
 %   ans =
-%   
+%
 %       -2
-% 
+%
 % ### Unique environments
-% 
+%
 % Set the 'shared' flag to false in order to evaluate each Julia call in a separate namespace on the Julia server:
-% 
+%
 %   % Restart the server, setting 'shared' to false
 %   >> JLCALL('LinearAlgebra.norm', {[3.0; 4.0]}, 'modules', {'LinearAlgebra'}, 'restart', true, 'shared', false)
-%   
+%
 %   ans =
-%   
+%
 %        5
-%   
+%
 %   % This call now errors, despite the above command loading the LinearAlgebra module, as LinearAlgebra.norm is evaluated in a new namespace
 %   >> JLCALL('LinearAlgebra.norm', {[3.0; 4.0]}, 'shared', false)
 %   ERROR: LoadError: UndefVarError: LinearAlgebra not defined
 %   Stacktrace:
 %    ...
-% 
+%
 % ### Unique Julia instances
-% 
+%
 % Instead of running Julia code on a persistent Julia server, unique Julia instances can be launched for each call to JLCALL by passing the 'server' flag with value false.
-% 
+%
 % NOTE: this may cause significant overhead when repeatedly calling JLCALL due to Julia package precompilation and loading:
-% 
+%
 %   >> tic; JLCALL('x -> sum(abs2, x)', {1:5}, 'server', false); toc
 %   Elapsed time is 4.181178 seconds. % call unique Julia instance
-%   
+%
 %   >> tic; JLCALL('x -> sum(abs2, x)', {1:5}, 'restart', true); toc
 %   Elapsed time is 5.046929 seconds. % re-initialize Julia server
-%   
+%
 %   >> tic; JLCALL('x -> sum(abs2, x)', {1:5}); toc
 %   Elapsed time is 0.267088 seconds. % call server; significantly faster
-% 
+%
 % ### Loading code from a local project
-% 
+%
 % Code from a local Julia project can be loaded and called:
-% 
+%
 %   >> JLCALL('MyProject.my_function', args, kwargs, ...
 %       'project', '/path/to/MyProject', ...
 %       'modules', {'MyProject'})
-% 
+%
 % NOTE: the string passed via the 'project' flag is simply forwarded to Pkg.activate; it is the user's responsibility to ensure that the project's dependencies have been installed.
-% 
+%
 % ### Loading setup code
-% 
+%
 % Julia functions may require or return types which cannot be directly passed from or loaded into MATLAB.
 % For example, suppose one would like to query Base.VERSION.
 % Naively calling JLCALL('() -> Base.VERSION') would fail, as typeof(Base.VERSION) is not a String but a VersionNumber.
-% 
+%
 % One possible remedy is to define a wrapper function in a Julia script:
-% 
+%
 %   # setup.jl
 %   julia_version() = string(Base.VERSION)
-% 
+%
 % Then, use the 'setup' flag to pass the above script to JLCALL:
-% 
+%
 %   >> JLCALL('julia_version', 'setup', '/path/to/setup.jl')
-%   
+%
 %   ans =
-%   
+%
 %       '1.6.1'
-% 
+%
 % In this case, JLCALL('() -> string(Base.VERSION)') would work just as well.
 % In general, however, interfacing with complex Julia libraries using MATLAB types may be nontrivial, and the 'setup' flag allows for the execution of arbitrary setup code.
-% 
+%
 % NOTE: the setup script is loaded into the global scope using include; when using persistent environments, symbols defined in the setup script will be available on subsequent calls to JLCALL.
-% 
+%
 % ### Handling Julia outputs
-% 
+%
 % Output(s) from Julia are returned using the MATLAB cell array varargout, MATLAB's variable-length list of output arguments.
 % A helper function MATDaemon.matlabify is used to convert Julia values into MATLAB-compatible values.
 % Specifically, the following rules are used to populate varargout with the Julia output y:
-% 
+%
 % 1. If y::Nothing, then varargout = {} and no outputs are returned to MATLAB
 % 2. If y::Tuple, then length(y) outputs are returned, with varargout{i} given by matlabify(y[i])
 % 3. Otherwise, one output is returned with varargout{1} given by matlabify(y)
-% 
+%
 % The following matlabify methods are defined by default:
-% 
+%
 %   matlabify(x) = x # default fallback
 %   matlabify(::Union{Nothing, Missing}) = zeros(0,0) # equivalent to MATLAB's []
 %   matlabify(x::Symbol) = string(x)
 %   matlabify(xs::Tuple) = Any[matlabify(x) for x in xs] # matlabify values
 %   matlabify(xs::Union{<:AbstractDict, <:NamedTuple, <:Base.Iterators.Pairs}) = Dict{String, Any}(string(k) => matlabify(v) for (k, v) in pairs(xs)) # convert keys to strings and matlabify values
-% 
+%
 % NOTE: MATLAB cell and struct types correspond to Array{Any} and Dict{String, Any} in Julia.
-% 
+%
 % Conversion via matlabify can easily be extended to additional types.
 % Returning to the example from the above section, we can define a matlabify method for Base.VersionNumber:
-% 
+%
 %   # setup.jl
 %   MATDaemon.matlabify(v::Base.VersionNumber) = string(v)
-% 
+%
 % Now, the return type will be automatically converted:
-% 
+%
 %   >> JLCALL('() -> Base.VERSION', 'setup', '/path/to/setup.jl')
-%   
+%
 %   ans =
-%   
+%
 %       '1.6.1'
-% 
+%
 % ### Performance
-% 
+%
 % MATLAB inputs and Julia ouputs are passed back and forth between MATLAB and the DaemonMode.jl server by writing to temporary .mat files.
 % The location of these files can be configured with the 'infile' and 'outfile' flags, respectively.
 % Pointing these files to a ram-backed file system is recommended when possible (for example, the /tmp folder on Linux is usually ram-backed), as read/write speed will likely improve.
 % This is now the default; 'infile' and 'outfile' are created via the MATLAB tempname function (thanks to @mauro3 for this tip).
-% 
+%
 % Nevertheless, this naturally leads to some overhead when calling Julia, particularly when the MATLAB inputs and/or Julia outputs have large memory footprints.
 % It is therefore not recommended to use JLCALL in performance critical loops.
-% 
+%
 % ## MATLAB and Julia version compatibility
-% 
+%
 % This package has been tested on a variety of MATLAB versions.
 % However, for some versions of Julia and MATLAB, supported versions of external libraries may clash.
 % For example, running JLCALL using Julia v1.6.1 and MATLAB R2015b gives the following error:
-% 
+%
 %   >> JLCALL
-%   
+%
 %   ERROR: Unable to load dependent library ~/.local/julia-1.6.1/bin/../lib/julia/libjulia-internal.so.1
-%   
+%
 %   Message: /usr/local/MATLAB/R2015b/sys/os/glnxa64/libstdc++.so.6: version GLIBCXX_3.4.20' not found (required by ~/.local/julia-1.6.1/bin/../lib/julia/libjulia-internal.so.1)
-% 
+%
 % This error results due to a clash of supported libstdc++ versions, and does not occur when using e.g. Julia v1.5.4 with MATLAB R2015b, or Julia v1.6.1 with MATLAB R2020b.
-% 
+%
 % If you encounter this issue, see the <a href="matlab: web('https://github.com/JuliaLang/julia/blob/master/doc/build/build.md#required-build-tools-and-external-libraries')">Julia</a> and <a href="matlab: web('https://www.mathworks.com/support/requirements/supported-compilers.html')">MATLAB</a> documentation for information on mutually supported external libraries.
-% 
+%
 % ## About this package
-% 
+%
 % This repository contains utilities for parsing and running Julia code, passing MATLAB arguments to Julia, and retrieving Julia outputs from MATLAB.
-% 
+%
 % The workhorse behind MATDaemon.jl and JLCALL is <a href="matlab: web('https://github.com/dmolina/DaemonMode.jl')">DaemonMode.jl</a> which is used to start a persistent Julia server in the background.
 
     % Parse inputs
@@ -484,7 +484,7 @@ function try_run(opts, script, mode, msg)
 
     % Build and run Julia command
     cmd = [opts.runtime, ' ', flags, ' ', script, detach];
-    st = system(cmd);
+    [st, ~] = system(cmd);
 
     if opts.debug
         fprintf('* %s (status = %d):\n*   %s\n\n', msg, st, cmd);
@@ -506,7 +506,20 @@ function runtime = try_find_julia_runtime()
             return % default to 'julia'
         end
         if st == 0
-            runtime = strtrim(res);
+            runtime = splitlines(strtrim(res));
+            if numel(runtime) > 1
+                runtime_list = '';
+                for ii = 1:numel(runtime)
+                    runtime_list = [runtime_list, num2str(ii), '. ', runtime{ii}, newline]; %#ok
+                end
+                msg = {
+                    'Found multiple Julia binaries:\n%s'
+                    'Defaulting to the first binary path:\n%s'
+                    '\nUse the ''runtime'' flag to explicitly set the ''julia'' binary path and suppress this warning.\n'
+                };
+                warning(strjoin(msg, newline), runtime_list, runtime{1})
+            end
+            runtime = runtime{1};
         end
     catch me
         % ignore error; default to 'julia'
@@ -521,6 +534,7 @@ function tmp = workspace_tempname(opts)
     [dirname, filename] = fileparts(tempname(tmp_dir));
 
     persistent filecount
+
     if isempty(filecount)
         filecount = 0;
     else
